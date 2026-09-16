90005357fce224aab15cf49fabef95834dce0849
ozanstn1-stack <ozanstn1-stack@users.noreply.github.com>
2026-09-16 21:04:52 +0300

    Bump version to 1.0.1 (versionCode 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.aivideostudio"
         minSdk = 29
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
